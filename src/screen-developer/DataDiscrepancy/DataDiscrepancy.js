e40e027e44aae7f2e0d318f0e2aa75b55bda3212
Saswati Panda <panda.sas20@gmail.com>
2023-05-11 11:19:45 -0500

    Added contact information on all feedback pages

--- src/screen-developer/DataDiscrepancy/DataDiscrepancy.js
+++ src/screen-developer/DataDiscrepancy/DataDiscrepancy.js
@@ -4,10 +4,14 @@ import EmbeddedHelp from "../../app/common/EmbeddedHelp/EmbeddedHelp";
 const DataDiscrepancy = () => {
   return (
     <div className="flex w-full flex-column pl-6">
-      <h2>Data Discrepancy Template</h2>
+      <h2>Data Discrepancy</h2>
       <EmbeddedHelp>
         Thank you for your time! Feedbacks helps us improve.
       </EmbeddedHelp>
+      <p>
+        <br />- Please email <b> steve.berthel@panoramaglobal.org</b> to report
+        any data discrepancy.
+      </p>
     </div>
   );
 };
