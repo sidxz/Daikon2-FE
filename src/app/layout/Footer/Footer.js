6ef238f9ec552b2df064cd2571b6a4fcb71f1d86
Siddhant Rath <sid@tamu.edu>
2023-06-23 18:41:26 -0500

    interm

--- src/app/layout/Footer/Footer.js
+++ src/app/layout/Footer/Footer.js
@@ -1,25 +1,30 @@
+import { Tag } from "primereact/tag";
 import React from "react";
 import { appVersion } from "../../../appVersion";
 import cssClass from "./Footer.module.css";
 
 const Footer = () => {
   return (
-
     <div className={cssClass.Footer}>
       <hr className={cssClass.Hr} />
-      <div className={["flex flex-column", cssClass.Text].join(" ")}>
-
+      <div className={["flex flex-column gap-2", cssClass.Text].join(" ")}>
         <div className="flex justify-content-end">
-          &copy; | DAIKON {appVersion.stream} {appVersion.release} {appVersion.channel}
+          <p style={{ margin: "0px" }}>
+            &copy; | DAIKON {appVersion.stream}{" "}
+            <b>{"  " + appVersion.release + " "}</b>
+            {appVersion.channel}
+          </p>
         </div>
-        <div className="flex justify-content-end">
-          <a href="/">Contact</a> | <a href="/">Disclaimer</a>
+        <div className="flex justify-content-end gap-2">
+          <Tag
+            value="Cite DAIKON"
+            severity="success"
+            onClick={() => console.log("/cite")}
+          />
+          <Tag value="TBDA Confidential" severity="danger" />
         </div>
-
-
       </div>
     </div>
-
   );
 };
 
