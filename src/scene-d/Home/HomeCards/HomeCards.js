431170f5b35855704b64450598bd7c59418a72e1
Saswati Panda <panda.sas20@gmail.com>
2023-06-23 18:34:47 -0500

    Updated HomeCards.js

--- src/scene-d/Home/HomeCards/HomeCards.js
+++ src/scene-d/Home/HomeCards/HomeCards.js
@@ -1,10 +1,8 @@
-import { Card } from "primereact/card";
-import React, { useContext } from 'react';
-import { NavLink } from "react-router-dom";
-import { RootStoreContext } from '../../../app/stores/rootStore';
+import React, { useContext } from "react";
+import { RootStoreContext } from "../../../app/stores/rootStore";
+import HomeCardsCircles from "./HomeCardsCircles/HomeCardsCircles";
 
 const HomeCards = () => {
-
   //const header = <img alt="Card" src="/images/usercard.png" />;
   const header = null;
 
@@ -12,104 +10,46 @@ const HomeCards = () => {
   const { appVars } = rootStore.generalStore;
 
   return (
-    <div className="flex flex-column">
-      <div className="flex justify-content-center gap-3 mb-3">
-        <div className="flex">
-          <NavLink to="/d/gene/" style={{ textDecoration: "None" }}>
-            <Card
-              title="Genes"
-              subTitle={'(' + appVars.appCount.geneCount + ')'}
-              style={{ width: "18em" }}
-              header={header}
-            >
-              <p className="p-m-0" style={{ lineHeight: "1.5" }}>
-                Shows a list of candidate genes &nbsp;
-                <i className="pi pi-arrow-right"></i>
-              </p>
-            </Card>
-          </NavLink>
-        </div>
-        <div className="flex">
-          <NavLink to="/d/target/" style={{ textDecoration: "None" }}>
-            <Card
-              title="Targets"
-              subTitle={'(' + appVars.appCount.targetCount + ')'}
-              style={{ width: "18em" }}
-              header={header}
-            >
-              <p className="p-m-0" style={{ lineHeight: "1.5" }}>
-                Shows a list of targets&nbsp;
-                <i className="pi pi-arrow-right"></i>
-              </p>
-            </Card>
-          </NavLink>
-        </div>
-        <div className="flex">
-          <NavLink to="/d/screen/" style={{ textDecoration: "None" }}>
-            <Card
-              title="Screens"
-              subTitle={'(' + appVars.appCount.screenCount + ')'}
-              style={{ width: "18em" }}
-              header={header}
-            >
-              <p className="p-m-0" style={{ lineHeight: "1.5" }}>
-                Shows a list of screens &nbsp;
-                <i className="pi pi-arrow-right"></i>
-              </p>
-            </Card>
-          </NavLink>
-        </div>
+    <div className="flex justify-content-center gap-5 mt-2 ">
+      <div className="flex">
+        <HomeCardsCircles
+          icon="icon icon-conceptual icon-dna"
+          total={appVars.appCount.geneCount}
+        />
       </div>
-      <div className="flex justify-content-center gap-3">
-
-        <div className="flex">
-          <NavLink to="/d/ha/" style={{ textDecoration: "None" }}>
-            <Card
-              title="HAs"
-              subTitle={'(' + appVars.appCount.haCount + ')'}
-              style={{ width: "18em" }}
-              header={header}
-            >
-              <p className="p-m-0" style={{ lineHeight: "1.5" }}>
-                Shows a list of all hit assessments &nbsp;
-                <i className="pi pi-arrow-right"></i>
-              </p>
-            </Card>
-          </NavLink>
-        </div>
-        <div className="flex">
-          <NavLink to="/d/portfolio/" style={{ textDecoration: "None" }}>
-            <Card
-              title="Portfolio"
-              subTitle={'(' + appVars.appCount.portfolioCount + ')'}
-              style={{ width: "18em" }}
-              header={header}
-            >
-              <p className="p-m-0" style={{ lineHeight: "1.5" }}>
-                Shows a list of all portfolios &nbsp;
-                <i className="pi pi-arrow-right"></i>
-              </p>
-            </Card>
-          </NavLink>
-        </div>
-        <div className="flex">
-          <NavLink to="/d/post-portfolio/" style={{ textDecoration: "None" }}>
-            <Card
-              title="Post Portfolio"
-              subTitle={'(' + appVars.appCount.postPortfolioCount + ')'}
-              style={{ width: "18em" }}
-              header={header}
-            >
-              <p className="p-m-0" style={{ lineHeight: "1.5" }}>
-                Shows a list of all post portfolios &nbsp;
-                <i className="pi pi-arrow-right"></i>
-              </p>
-            </Card>
-          </NavLink>
-        </div>
+      <div className="flex">
+        <HomeCardsCircles
+          icon="icon icon-common icon-target"
+          total={appVars.appCount.targetCount}
+          // active="105"
+        />
+      </div>
+      <div className="flex">
+        <HomeCardsCircles
+          icon="icon icon-common icon-search"
+          total={appVars.appCount.screenCount}
+        />
+      </div>
+      <div className="flex">
+        <HomeCardsCircles
+          icon="icon icon-conceptual icon-chemical"
+          total={appVars.appCount.haCount}
+        />
+      </div>
+      <div className="flex">
+        <HomeCardsCircles
+          icon="icon icon-common icon-analyse"
+          total={appVars.appCount.portfolioCount}
+        />
+      </div>
+      <div className="flex">
+        <HomeCardsCircles
+          icon="icon icon-common icon-drug"
+          total={appVars.appCount.postPortfolioCount}
+        />
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default HomeCards
+export default HomeCards;
