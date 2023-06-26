94676039f92550ea89083e5d6439da3f050e7155
Saswati Panda <panda.sas20@gmail.com>
2023-06-26 09:42:28 -0500

    Updated Home.js for discussion section

--- src/scene-d/Home/Home.js
+++ src/scene-d/Home/Home.js
@@ -4,7 +4,7 @@ import HomeCards from "./HomeCards/HomeCards";
 
 const Home = () => {
   return (
-    <div className="Home flex flex-column align-items-center w-full pb-4">
+    <div className="Home flex flex-column align-items-center w-full pb-4 pl-5 pr-5 ml-5 mr-5">
       <div className="flex">
         <h1>
           Data Acquisition, Integration and Knowledge capture application
@@ -14,7 +14,10 @@ const Home = () => {
       <div className="flex">
         <HomeCards />
       </div>
-      <div className="flex"></div>
+      <div className="flex w-full justify-content-center gap-5 mt-2 border-3">
+        <div className="flex m-1 p-2 border-3">Home</div>
+        <div className="flex m-1 p-2 border-3">Home</div>
+      </div>
     </div>
   );
 };
---
+++ src/scene-d/Home/HomeDiscussionCards/HomeDiscussionCard.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const HomeDiscussionCard = () => {
+  return <div className="flex justify-content-center gap-5 mt-2 "></div>;
+};
+
+export default HomeDiscussionCard;
