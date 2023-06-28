897ed47e975a747ed34f7776ac6d04cf73031b00
Saswati Panda <panda.sas20@gmail.com>
2023-06-28 10:36:02 -0500

    New section added for Whats New

---
+++ src/scene-d/Home/HomeWhatsNew/HomeWhatsNew.js
@@ -0,0 +1,17 @@
+import { Card } from "primereact/card";
+import React from "react";
+
+const HomeWhatsNew = () => {
+  return (
+    <Card title="📣 Attention TBDA Group! 🧪🔬">
+      <p className="m-0">
+        {" "}
+        We are seeking your expertise to advance our collective mission to make
+        DAIKON impactful. We invite you to contribute all available data on your
+        screening efforts and validated hits achieved thus far.
+      </p>
+    </Card>
+  );
+};
+
+export default HomeWhatsNew;
