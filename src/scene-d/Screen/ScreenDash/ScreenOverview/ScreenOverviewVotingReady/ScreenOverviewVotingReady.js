10d2039af028b4270e82539c575259d51d2ded78
Saswati Panda <panda.sas20@gmail.com>
2023-07-03 14:58:32 -0500

    Updated Voting Ready component

--- src/scene-d/Screen/ScreenDash/ScreenOverview/ScreenOverviewVotingReady/ScreenOverviewVotingReady.js
+++ src/scene-d/Screen/ScreenDash/ScreenOverview/ScreenOverviewVotingReady/ScreenOverviewVotingReady.js
@@ -3,19 +3,14 @@ import React from "react";
 const ScreenOverviewVotingReady = ({ votingReady }) => {
   let votingReadyComponent = votingReady.map((voteReady) => {
     return (
-      <div
-        className="flex justify-content-center"
-        style={{
-          fontSize: "medium",
-        }}
-      >
-        {voteReady}
+      <div className="border-circle w-5rem h-5rem ml-1 mr-1 bg-white flex items-center justify-content-center ">
+        <div className="flex align-items-center">{voteReady}</div>
       </div>
     );
   });
 
   return (
-    <div className="flex ml-2 mt-2 w-full surface-400 flex-column gap-3 mb-3">
+    <div className="flex ml-2 mt-2 w-full surface-400 flex-column gap-2 mb-3">
       <div
         className="flex p-2 bg-green-100 justify-content-center "
         style={{
@@ -24,7 +19,7 @@ const ScreenOverviewVotingReady = ({ votingReady }) => {
       >
         VOTING READY
       </div>
-      <div className="flex gap-5 m-5 justify-content-center">
+      <div className="flex gap-2 mb-2 justify-content-center">
         {votingReadyComponent}{" "}
       </div>
     </div>
