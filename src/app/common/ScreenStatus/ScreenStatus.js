f43c664ab4b2344931546d6a89ab68f1b5ec3110
Siddhant Rath <sid@tamu.edu>
2023-07-12 13:56:13 -0500

    intermediate

--- src/app/common/ScreenStatus/ScreenStatus.js
+++ src/app/common/ScreenStatus/ScreenStatus.js
@@ -1,72 +1,52 @@
-import { Button } from "primereact/button";
-import { Inplace, InplaceContent, InplaceDisplay } from "primereact/inplace";
-import React, { useContext, useRef, useState } from "react";
-
-import { Steps } from "primereact/steps";
+import { Dropdown } from "primereact/dropdown";
+import React, { useContext } from "react";
+import { FcDataSheet, FcNeutralTrading, FcOk, FcPlanner } from "react-icons/fc";
+import { GiVote } from "react-icons/gi";
 import { RootStoreContext } from "../../stores/rootStore";
 import "./ScreenStatus.css";
 
 const ScreenStatus = ({ status }) => {
   const rootStore = useContext(RootStoreContext);
   const { appVars } = rootStore.generalStore;
-  const op = useRef(null);
-  const [editActive, setEditActive] = useState(false);
 
-  console.log(appVars);
-  const items = [
-    ...appVars.screenStatuses.map((s) => {
-      return { label: s };
-    }),
+  let indexMap = {
+    Planned: 0,
+    "Assay Development": 1,
+    Ongoing: 2,
+    "Voting Ready": 3,
+    Completed: 4,
+  };
+
+  const optionsWithIcons = [
+    { name: "Planned", icon: <FcPlanner /> },
+    { name: "Assay Development", icon: <FcDataSheet /> },
+    { name: "Ongoing", icon: <FcNeutralTrading /> },
+    { name: "Voting Ready", icon: <GiVote /> },
+    { name: "Completed", icon: <FcOk /> },
   ];
 
-  let stepsView = (
-    <div className="flex justify-content-end" style={{ minWidth: "600px" }}>
-      <div className="flex w-full">
-        <Steps model={items} className="w-full gap-2 p-0 m-0" />
-      </div>
-      <div className="flex">
-        <Button
-          icon="icon icon-common icon-angle-right"
-          onClick={() => setEditActive(!editActive)}
-          className="p-button-text p-button-plain m-0"
-        ></Button>
-      </div>
-    </div>
-  );
-
-  let chipTemplate = (
-    <div>
-      {" "}
-      <Button
-        label={status}
-        icon="pi pi-refresh"
-        onClick={(e) => op.current.toggle(e)}
-        aria-haspopup
-        aria-controls="overlay_panel"
-        className="p-button-text p-button-sm p-button-plain"
-      ></Button>
-    </div>
-  );
+  const selectedItemTemplate = (option) => {
+    if (option) {
+      return (
+        <div className="flex align-items-center gap-2">
+          <div className="flex flex-column">{option.icon}</div>
+          <div className="flex flex-column">{option.name}</div>
+        </div>
+      );
+    }
+  };
 
   return (
     <div className="flex">
-      <Inplace active={editActive} onToggle={(e) => setEditActive(e.value)}>
-        <InplaceDisplay className="m-0">
-          <div class="flex fadein animation-duration-1000 ">
-            <Button
-              label={status}
-              icon="icon icon-common icon-angle-left"
-              className="p-button-text p-button-plain m-0 p-button-rounded"
-            ></Button>
-          </div>
-        </InplaceDisplay>
-
-        <InplaceContent className="m-0">
-          <div class="fadein animation-duration-1000 flex w-full">
-            {stepsView}
-          </div>
-        </InplaceContent>
-      </Inplace>
+      <Dropdown
+        value="Planned"
+        optionLabel="name"
+        optionValue="name"
+        options={optionsWithIcons}
+        placeholder="Set Status"
+        itemTemplate={selectedItemTemplate}
+        valueTemplate={selectedItemTemplate}
+      />
     </div>
   );
 };
