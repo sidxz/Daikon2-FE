0d446efc1631f674a2af6cf5dc7d8e533af817b2
Siddhant Rath <sid@tamu.edu>
2024-02-07 13:16:47 -0600

    hello world

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import Login from "./Auth/Login/Login";
+
+const App = () => {
+  return (
+    <>
+      <h1>Hello, world!</h1>
+      <Login />
+    </>
+  );
+};
+
+export default App;
