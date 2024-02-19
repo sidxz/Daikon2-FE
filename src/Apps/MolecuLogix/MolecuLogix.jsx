4171d105cb53c3e85b5fc33b06cf0e1ec75c150a
Saswati Panda <panda.sas20@gmail.com>
2024-02-19 09:54:24 -0600

    cap bug fix

--- src/Apps/MolecuLogix/MolecuLogix.jsx
+++ src/Apps/MolecuLogix/MolecuLogix.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 import { Route, Routes } from "react-router";
 import MLogixDash from "./MLogixDash/MLogixDash";
+import MLogixDraw from "./MLogixDraw/MLogixDraw";
 import MLogixMenuBar from "./MLogixMenuBar/MLogixMenuBar";
-import MLogixDraw from "./MlogixDraw/MLogixDraw";
 
 const MolecuLogix = () => {
   return (
