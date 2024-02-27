b8c11d6e321c9f9159bfeeb5546ce2092f985b8e
Saswati Panda <panda.sas20@gmail.com>
2024-02-27 16:13:35 -0600

    home landing page

--- src/Apps/Flow/FlowDash/FlowDash.jsx
+++ src/Apps/Flow/FlowDash/FlowDash.jsx
@@ -1,7 +1,17 @@
 import React from "react";
+import FlowDashCards from "./FlowDashCards/FlowDashCards";
 
 const FlowDash = () => {
-  return <div>FlowDash</div>;
+  return (
+    <div className="Home surface-50 flex flex-column align-items-center w-full pb-4 pl-5 pr-5 ml-5 mr-5">
+      <div className="flex">
+        <h1>Data Acquisition, Integration and Knowledge Capture Application</h1>
+      </div>
+      <div className="flex">
+        <FlowDashCards />
+      </div>
+    </div>
+  );
 };
 
 export default FlowDash;
