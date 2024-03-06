401259dc58530616ed95798bf41f9fe6cd956782
Saswati Panda <panda.sas20@gmail.com>
2024-03-06 13:40:53 -0600

    add library screen

--- src/Apps/Flow/FlowScreen/FSPhViewer/FSPhVScreen/FSPhVScreen.jsx
+++ src/Apps/Flow/FlowScreen/FSPhViewer/FSPhVScreen/FSPhVScreen.jsx
@@ -1,50 +1,236 @@
+import { observer } from "mobx-react-lite";
+import { BlockUI } from "primereact/blockui";
 import { BreadCrumb } from "primereact/breadcrumb";
+import { Button } from "primereact/button";
 import { Chip } from "primereact/chip";
-import React, { useContext } from "react";
+import { Column } from "primereact/column";
+import { confirmDialog } from "primereact/confirmdialog";
+import { DataTable } from "primereact/datatable";
+import { Sidebar } from "primereact/sidebar";
+import React, { useContext, useState } from "react";
 import { useNavigate } from "react-router-dom";
+import Loading from "../../../../../Library/Loading/Loading";
 import SecHeading from "../../../../../Library/SecHeading/SecHeading";
 import { RootStoreContext } from "../../../../../RootStore";
+import { CalendarRowEditor } from "../../../../../Shared/TableRowEditors/CalendarRowEditor";
+import { ScientistRowEditor } from "../../../../../Shared/TableRowEditors/ScientistRowEditor";
+import { TextRowEditor } from "../../../../../Shared/TableRowEditors/TextRowEditor";
 import { appColors } from "../../../../../constants/colors";
 import { FormatScreeningMethod } from "../../shared/Formatters";
 import * as Helper from "./FSPhVScreenHelper";
+import FSPhScreenRunAdd from "./components/FSPhScreenRunAdd";
+import FSPhVScreenDataTableHeader from "./components/FSPhVScreenDataTableHeader";
 
 const FSPhVScreen = ({}) => {
+  const navigate = useNavigate();
   const rootStore = useContext(RootStoreContext);
 
-  const { selectedScreen } = rootStore.screenStore;
-  const { navigate } = useNavigate();
+  const { fetchScreen, isFetchingScreen, selectedScreen } =
+    rootStore.screenStore;
 
-  if (selectedScreen) {
+  const {
+    updateScreenRun,
+    isUpdatingScreenRun,
+    isAddingScreenRun,
+    isDeletingScreenRun,
+    deleteScreenRun,
+  } = rootStore.screenRunStore;
+
+  const [displayAddScreenSeqSideBar, setDisplayAddScreenSeqSideBar] =
+    useState(false);
+
+  if (isFetchingScreen) {
+    return <Loading message={"Fetching Screen..."} />;
+  }
+
+  const addScreenSeqSideBarHeader = (
+    <div className="flex align-items-center gap-2">
+      <i className="icon icon-common icon-plus-circle"></i>
+      <span className="font-bold">Add Screen Run</span>
+    </div>
+  );
+
+  const deleteBodyTemplate = (rowData) => {
+    const accept = () => {
+      // Delete hit
+      deleteScreenRun(rowData.id);
+    };
+    const reject = () => {
+      // Do nothing
+    };
     return (
-      <div className="flex flex-column w-full">
-        <div className="flex w-full">
-          <BreadCrumb
-            model={Helper.breadCrumbItems(selectedScreen, navigate)}
-          />
+      <Button
+        severity="danger"
+        icon="ri-delete-bin-2-line"
+        className="p-button-text"
+        onClick={() => {
+          confirmDialog({
+            message: "Do you want to delete this record?",
+            header: "Delete Confirmation",
+            icon: "pi pi-info-circle",
+            defaultFocus: "reject",
+            acceptClassName: "p-button-danger",
+            accept,
+            reject,
+          });
+        }}
+      />
+    );
+  };
+
+  if (selectedScreen && !isFetchingScreen) {
+    return (
+      <>
+        <div className="flex flex-column w-full">
+          <div className="flex w-full">
+            <BreadCrumb
+              model={Helper.breadCrumbItems(selectedScreen, navigate)}
+            />
+          </div>
+          <div className="flex w-full">
+            <SecHeading
+              icon="icon icon-conceptual icon-structures-3d"
+              heading={"Screen - " + selectedScreen.name}
+              displayHorizon={true}
+              color={appColors.sectionHeadingBg.screen}
+              customElements={[
+                <Chip
+                  label={FormatScreeningMethod(selectedScreen?.method)}
+                  icon="icon icon-common icon-circle-notch"
+                />,
+                <Chip
+                  label={selectedScreen?.primaryOrgName}
+                  icon="ri-organization-chart"
+                  className="mr-3"
+                />,
+              ]}
+            />
+          </div>
+
+          <div className="flex w-full">
+            <BlockUI
+              blocked={
+                isUpdatingScreenRun || isAddingScreenRun || isDeletingScreenRun
+              }
+              containerClassName="w-full"
+            >
+              <DataTable
+                className="p-datatable-gridlines w-full"
+                size="small"
+                value={selectedScreen?.screenRuns}
+                showGridlines
+                editMode="row"
+                sortField="startDate"
+                sortOrder={-1}
+                onRowEditComplete={(e) => updateScreenRun(e.newData)}
+                //loading={isEditingScreenSequence}
+
+                header={
+                  <FSPhVScreenDataTableHeader
+                    selectedScreen={selectedScreen}
+                    setDisplayAddScreenSeqSideBar={
+                      setDisplayAddScreenSeqSideBar
+                    }
+                  />
+                }
+              >
+                <Column
+                  field="library"
+                  header="Library"
+                  editor={(options) => TextRowEditor(options)}
+                />
+
+                <Column
+                  field="librarySize"
+                  header="Library Size"
+                  editor={(options) => TextRowEditor(options)}
+                />
+
+                <Column
+                  field={"protocol"}
+                  //body={protocolBodyTemplate}
+                  header="Protocol"
+                  editor={(options) => TextRowEditor(options)}
+                />
+                <Column
+                  field="concentration"
+                  header="Inhibitor C (&micro;M)"
+                  editor={(options) => TextRowEditor(options)}
+                />
+                <Column
+                  field="noOfCompoundsScreened"
+                  header="# Compounds Screened"
+                  editor={(options) => TextRowEditor(options)}
+                  //body={CompoundsScreenedTemplate}
+                />
+                <Column
+                  field="scientist"
+                  header="Scientist"
+                  editor={(options) => ScientistRowEditor(options)}
+                  style={{ wordWrap: "break-word" }}
+                />
+                <Column
+                  field="startDate"
+                  header="Start Date"
+                  editor={(options) => CalendarRowEditor(options)}
+                  body={Helper.StartDateTemplate}
+                  sortable
+                />
+                <Column
+                  field="endDate"
+                  header="End Date"
+                  editor={(options) => CalendarRowEditor(options)}
+                  body={Helper.EndDateTemplate}
+                  sortable
+                />
+                <Column
+                  field="primaryHitCount"
+                  header="# Initial Hits"
+                  editor={(options) => TextRowEditor(options)}
+                  // body={UnverifiedHitCountTemplate}
+                />
+
+                <Column
+                  field="confirmedHitCount"
+                  header="# Confirmed Hits"
+                  editor={(options) => TextRowEditor(options)}
+                  //body={ConfirmedHitCountTemplate}
+                />
+
+                <Column
+                  field="hitRate"
+                  header="Hit Rate"
+                  editor={(options) => TextRowEditor(options)}
+                  //body={ConfirmedHitCountTemplate}
+                />
+
+                <Column
+                  rowEditor
+                  headerStyle={{ minWidth: "4rem" }}
+                  bodyStyle={{ textAlign: "center" }}
+                />
+                <Column body={deleteBodyTemplate} header="Delete" />
+              </DataTable>
+            </BlockUI>
+          </div>
         </div>
-        <div className="flex w-full">
-          <SecHeading
-            icon="icon icon-conceptual icon-structures-3d"
-            heading={"Screen - " + selectedScreen.name}
-            displayHorizon={true}
-            color={appColors.sectionHeadingBg.screen}
-            customElements={[
-              <Chip
-                label={FormatScreeningMethod(selectedScreen?.method)}
-                icon="icon icon-common icon-circle-notch"
-              />,
-              <Chip
-                label={selectedScreen?.primaryOrgName}
-                icon="ri-organization-chart"
-                className="mr-3"
-              />,
-            ]}
+        <Sidebar
+          visible={displayAddScreenSeqSideBar}
+          position="right"
+          onHide={() => setDisplayAddScreenSeqSideBar(false)}
+          className="p-sidebar-sm"
+          header={addScreenSeqSideBarHeader}
+        >
+          <FSPhScreenRunAdd
+            closeSideBar={() => setDisplayAddScreenSeqSideBar(false)}
+            screenId={selectedScreen?.id}
           />
-        </div>
-      </div>
+        </Sidebar>
+      </>
     );
   }
-  return <div>FSPhVScreens</div>;
+
+  return <div>FSPhVScreen</div>;
 };
 
-export default FSPhVScreen;
+export default observer(FSPhVScreen);
---
+++ src/Apps/Flow/FlowScreen/FSPhViewer/FSPhVScreen/components/FSPhScreenRunAdd.jsx
@@ -0,0 +1,311 @@
+import { useFormik } from "formik";
+import { observer } from "mobx-react-lite";
+import { Button } from "primereact/button";
+import { Calendar } from "primereact/calendar";
+import { InputText } from "primereact/inputtext";
+import { InputTextarea } from "primereact/inputtextarea";
+import { classNames } from "primereact/utils";
+import React, { useContext } from "react";
+import { RootStoreContext } from "../../../../../../RootStore";
+import InputScientist from "../../../../../../Shared/InputEditors/InputScientist";
+
+const FSPhScreenRunAdd = ({ screenId, closeSideBar }) => {
+  const rootStore = useContext(RootStoreContext);
+  const { isAddingScreenRun, addScreenRun } = rootStore.screenRunStore;
+
+  const formik = useFormik({
+    initialValues: {
+      library: "",
+      librarySize: "",
+      protocol: "",
+      scientist: "",
+      startDate: null,
+      endDate: null,
+      hitRate: "",
+      primaryHitCount: "",
+      confirmedHitCount: "",
+      noOfCompoundsScreened: "",
+      concentration: "",
+      notes: "",
+    },
+
+    validate: (values) => {
+      const errors = {};
+      if (!values.library) errors.library = "Library is required.";
+      // Additional validations can be added here
+      return errors;
+    },
+
+    onSubmit: (screenRunToAdd) => {
+      screenRunToAdd.screenId = screenId;
+      addScreenRun(screenRunToAdd).then(() => {
+        closeSideBar();
+        formik.resetForm();
+      });
+    },
+  });
+
+  // Helper functions for form validation and error messages
+  const isInvalid = (field) => formik.touched[field] && formik.errors[field];
+  const getErrorMessage = (field) =>
+    isInvalid(field) && (
+      <small className="p-error">{formik.errors[field]}</small>
+    );
+
+  return (
+    <div className="card w-full">
+      <form onSubmit={formik.handleSubmit} className="p-fluid">
+        <div className="field">
+          <label
+            htmlFor="library"
+            className={classNames({ "p-error": isInvalid("library") })}
+          >
+            Library
+          </label>
+          <InputText
+            id="library"
+            value={formik.values.library}
+            onChange={formik.handleChange}
+            className={classNames({
+              "p-invalid": isInvalid("library"),
+            })}
+          />
+          {getErrorMessage("library")}
+        </div>
+
+        <div className="field">
+          <label
+            htmlFor="librarySize"
+            className={classNames({ "p-error": isInvalid("librarySize") })}
+          >
+            Library Size
+          </label>
+          <InputText
+            id="librarySize"
+            value={formik.values.librarySize}
+            onChange={formik.handleChange}
+            className={classNames({
+              "p-invalid": isInvalid("librarySize"),
+            })}
+          />
+          {getErrorMessage("librarySize")}
+        </div>
+
+        <div className="field">
+          <label
+            htmlFor="protocol"
+            className={classNames({ "p-error": isInvalid("protocol") })}
+          >
+            Protocol
+          </label>
+          <InputTextarea
+            id="protocol"
+            value={formik.values.protocol}
+            onChange={formik.handleChange}
+            className={classNames({
+              "p-invalid": isInvalid("protocol"),
+            })}
+          />
+          {getErrorMessage("protocol")}
+        </div>
+
+        <div className="field">
+          <label
+            htmlFor="concentration"
+            className={classNames({ "p-error": isInvalid("concentration") })}
+          >
+            Concentration
+          </label>
+          <InputText
+            id="concentration"
+            value={formik.values.concentration}
+            onChange={formik.handleChange}
+            className={classNames({
+              "p-invalid": isInvalid("concentration"),
+            })}
+          />
+          {getErrorMessage("concentration")}
+        </div>
+
+        <div className="field">
+          <label
+            htmlFor="noOfCompoundsScreened"
+            className={classNames({
+              "p-error": isInvalid("noOfCompoundsScreened"),
+            })}
+          >
+            Total Compounds Screened
+          </label>
+          <InputText
+            id="noOfCompoundsScreened"
+            value={formik.values.noOfCompoundsScreened}
+            onChange={formik.handleChange}
+            className={classNames({
+              "p-invalid": isInvalid("noOfCompoundsScreened"),
+            })}
+          />
+          {getErrorMessage("noOfCompoundsScreened")}
+        </div>
+
+        <div className="field">
+          <label
+            htmlFor="startDate"
+            className={classNames({
+              "p-error": isInvalid("startDate"),
+            })}
+          >
+            Start Date
+          </label>
+          <Calendar
+            id="startDate"
+            name="startDate"
+            value={formik.values.startDate}
+            onChange={formik.handleChange}
+            dateFormat="dd/mm/yy"
+            mask="99/99/9999"
+            showIcon
+            className={classNames({
+              "p-invalid": isInvalid("startDate"),
+            })}
+          />
+
+          {getErrorMessage("startDate")}
+        </div>
+
+        <div className="field">
+          <label
+            htmlFor="endDate"
+            className={classNames({
+              "p-error": isInvalid("endDate"),
+            })}
+          >
+            End Date
+          </label>
+
+          <Calendar
+            id="endDate"
+            name="endDate"
+            value={formik.values.endDate}
+            onChange={formik.handleChange}
+            dateFormat="dd/mm/yy"
+            mask="99/99/9999"
+            showIcon
+            className={classNames({
+              "p-invalid": isInvalid("endDate"),
+            })}
+          />
+
+          {getErrorMessage("endDate")}
+        </div>
+
+        <div className="field">
+          <label
+            htmlFor="primaryHitCount"
+            className={classNames({
+              "p-error": isInvalid("primaryHitCount"),
+            })}
+          >
+            Initial Hit Count
+          </label>
+          <InputText
+            id="primaryHitCount"
+            value={formik.values.primaryHitCount}
+            onChange={formik.handleChange}
+            className={classNames({
+              "p-invalid": isInvalid("primaryHitCount"),
+            })}
+          />
+          {getErrorMessage("primaryHitCount")}
+        </div>
+
+        <div className="field">
+          <label
+            htmlFor="confirmedHitCount"
+            className={classNames({
+              "p-error": isInvalid("confirmedHitCount"),
+            })}
+          >
+            Confirmed Hit Count
+          </label>
+          <InputText
+            id="confirmedHitCount"
+            value={formik.values.confirmedHitCount}
+            onChange={formik.handleChange}
+            className={classNames({
+              "p-invalid": isInvalid("confirmedHitCount"),
+            })}
+          />
+          {getErrorMessage("confirmedHitCount")}
+        </div>
+
+        <div className="field">
+          <label
+            htmlFor="hitRate"
+            className={classNames({
+              "p-error": isInvalid("hitRate"),
+            })}
+          >
+            Hit Rate
+          </label>
+          <InputText
+            id="hitRate"
+            value={formik.values.hitRate}
+            onChange={formik.handleChange}
+            className={classNames({
+              "p-invalid": isInvalid("hitRate"),
+            })}
+          />
+          {getErrorMessage("hitRate")}
+        </div>
+
+        <div className="field">
+          <label
+            htmlFor="scientist"
+            className={classNames({ "p-error": isInvalid("scientist") })}
+          >
+            Scientist
+          </label>
+          <InputScientist
+            id="scientist"
+            value={formik.values.scientist}
+            onChange={formik.handleChange}
+            className={classNames({
+              "p-invalid": isInvalid("scientist"),
+            })}
+          />
+          {getErrorMessage("scientist")}
+        </div>
+
+        <div className="field">
+          <label
+            htmlFor="notes"
+            className={classNames({ "p-error": isInvalid("notes") })}
+          >
+            Notes
+          </label>
+          <InputText
+            id="notes"
+            value={formik.values.notes}
+            onChange={formik.handleChange}
+            className={classNames({
+              "p-invalid": isInvalid("notes"),
+            })}
+          />
+          {getErrorMessage("notes")}
+        </div>
+
+        <div className="flex justify-content-end">
+          <Button
+            icon="icon icon-common icon-database-submit"
+            type="submit"
+            label="Add to database"
+            className="p-button-secondary p-button-sm"
+            loading={isAddingScreenRun}
+          />
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default observer(FSPhScreenRunAdd);
