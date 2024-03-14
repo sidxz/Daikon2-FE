dd46c76993057fd4e436f2e3ac8fa83625c2b788
Saswati Panda <panda.sas20@gmail.com>
2024-03-14 13:59:27 -0500

    Flow Dash Cards icons

--- src/Apps/Flow/FlowDash/FlowDashCards/FlowDashCardsCircles/FlowDashCardsCircles.jsx
+++ src/Apps/Flow/FlowDash/FlowDashCards/FlowDashCardsCircles/FlowDashCardsCircles.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-const FlowDashCardsCircles = ({ icon, total, active }) => {
+const FlowDashCardsCircles = ({ svgIcon, total, active }) => {
   let activeComponent = (
     <div className="flex">
       <h3 style={{ margin: "0", padding: "0", color: "#44aa99" }}>Active</h3>
@@ -18,10 +18,10 @@ const FlowDashCardsCircles = ({ icon, total, active }) => {
           <div className="border-circle w-8rem h-8rem pt-1 bg-white flex items-center justify-content-center">
             <div className="flex flex-column align-items-center">
               <div className="flex align-items-center pt-3 pb-2">
-                <i
-                  className={icon}
-                  style={{ fontSize: "xx-large", color: "#3c83bd" }}
-                ></i>
+                <i style={{ fontSize: "xx-large", color: "#3c83bd" }}></i>
+                <div className="flex align-items-center justify-content-center text-3xl">
+                  {svgIcon ? svgIcon : <i className={icon} />}
+                </div>
               </div>
               {total && totalComponent}
               {active && activeComponent}
