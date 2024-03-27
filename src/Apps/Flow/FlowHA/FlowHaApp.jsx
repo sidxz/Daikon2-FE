60270fa87c5fd721713394be4bd8c081527651fa
Saswati Panda <panda.sas20@gmail.com>
2024-03-27 11:24:43 -0500

    case sensitivity fix

--- src/Apps/Flow/FlowHA/FlowHaApp.jsx
+++ src/Apps/Flow/FlowHA/FlowHaApp.jsx
@@ -2,17 +2,17 @@ import { observer } from "mobx-react-lite";
 import React from "react";
 import { Route, Routes } from "react-router";
 import { Navigate } from "react-router-dom";
-import FHANew from "./FHANew/FHANew";
-import FHAViewer from "./FHAViewer/FHAViewer";
 import FHaDashboard from "./FHaDashboard/FHaDashboard";
+import FHaNew from "./FHaNew/FHaNew";
+import FHaViewer from "./FHaViewer/FHaViewer";
 const FlowHaApp = () => {
   return (
     <>
       <Routes>
         <Route index element={<Navigate to="dash/" />} />
         <Route path="dash/*" element={<FHaDashboard />} />
-        <Route path="viewer/:id/*" element={<FHAViewer />} />
-        <Route path="new/*" element={<FHANew />} />
+        <Route path="viewer/:id/*" element={<FHaViewer />} />
+        <Route path="new/*" element={<FHaNew />} />
       </Routes>
     </>
   );
