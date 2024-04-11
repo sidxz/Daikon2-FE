4330df482c610ee2e2b52ce825fb2f60ef5a8bb4
Siddhant Rath <sid@tamu.edu>
2024-04-11 10:20:31 -0500

    moleculogix icon

--- src/Apps/MolecuLogix/Icons/MolecuLogixIcon.jsx
+++ src/Apps/MolecuLogix/Icons/MolecuLogixIcon.jsx
@@ -1,11 +1,11 @@
 import React from "react";
-import MolecuLogixSVG from "../../../assets/moleculogix/moleculogix.svg";
+import MolecuLogixSVG from "../../../assets/moleculogix/moleculogix2.svg";
 import "./MolecuLogixIcon.css";
 export const MolecuLogixIcon = ({ size }) => {
   const width = size || "24px";
   const height = size || "24px";
   return (
-    <div className="flex align-items-center ml-2 mr-2">
+    <div className="flex align-items-center ml-1 mr-1">
       <img
         src={MolecuLogixSVG}
         className="moleculogix-icon"
