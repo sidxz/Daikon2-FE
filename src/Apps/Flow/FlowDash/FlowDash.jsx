4a036bf70087c89a9094627abfd393adadf11197
Saswati Panda <panda.sas20@gmail.com>
2024-04-17 13:43:05 -0500

    whats new card in home landing

--- src/Apps/Flow/FlowDash/FlowDash.jsx
+++ src/Apps/Flow/FlowDash/FlowDash.jsx
@@ -1,8 +1,38 @@
+import { Card } from "primereact/card";
 import React from "react";
 import "./FlowDash.css";
 import FlowDashCards from "./FlowDashCards/FlowDashCards";
+import FlowDashWhatsNew from "./FlowDashWhatsNew/FlowDashWhatsNew";
 
 const FlowDash = () => {
+  let generateCardTitle = (icon, title) => {
+    return (
+      <div
+        className="flex justify-content-center p-0 gap-2"
+        style={{ margin: "0px", marginBottom: "-10px" }}
+      >
+        <div
+          className="flex p-0 m-0"
+          style={{
+            color: "#3c83bd",
+          }}
+        >
+          <i className={icon} />
+        </div>
+        <div className="flex p-0 m-0">
+          <h5
+            style={{
+              color: "#3c83bd",
+              margin: "0px",
+              padding: "0px",
+            }}
+          >
+            {title}
+          </h5>
+        </div>
+      </div>
+    );
+  };
   return (
     <div className="FlowDash bg-bluegray-50 flex flex-column align-items-center w-full">
       <div className="flex">
@@ -12,6 +42,17 @@ const FlowDash = () => {
       </div>
       <div className="flex">
         <FlowDashCards />
+      </div>
+      <div className="flex flex-column p-2 w-4">
+        <Card
+          title={generateCardTitle(
+            "icon icon-common icon-newspaper",
+            "What's New?"
+          )}
+          className="w-full min-h-full"
+        >
+          <FlowDashWhatsNew />
+        </Card>
       </div>
     </div>
   );
