3896a404a7e75180d1fd06ba69c90ce5cfa780ab
Siddhant Rath <sid@tamu.edu>
2024-06-10 13:55:13 -0500

    remove default configs

--- src/config/axiosConfig.jsx
+++
@@ -1,7 +0,0 @@
-export const AxiosConfig = {
-  baseURL: "http://localhost:8010", // If you have a base URL for your API
-  headers: {
-    Accept: "application/json", // Ensures you expect JSON response
-    "Content-Type": "application/json", // In case you send data to the server in JSON format
-  },
-};
