9487089f6ea6d87def6ffe1efa70158727e0b18a
Siddhant Rath <sid@tamu.edu>
2024-06-13 15:35:44 -0500

    Ha Org

--- src/Apps/Flow/FlowHA/FHaViewer/FHaVSettings/components/FHaVSettingsDates.jsx
+++ src/Apps/Flow/FlowHA/FHaViewer/FHaVSettings/components/FHaVSettingsDates.jsx
@@ -1,4 +1,5 @@
 import { Formik } from "formik";
+import { observer } from "mobx-react-lite";
 import { Button } from "primereact/button";
 import { Calendar } from "primereact/calendar";
 import React, { useContext } from "react";
@@ -116,4 +117,4 @@ const FHaVSettingsDates = ({}) => {
   );
 };
 
-export default FHaVSettingsDates;
+export default observer(FHaVSettingsDates);
---
+++ src/Apps/Flow/FlowHA/FHaViewer/FHaVSettings/components/FHaVSettingsOrgs.jsx
@@ -0,0 +1,102 @@
+import { Formik } from "formik";
+import { observer } from "mobx-react-lite";
+import { Button } from "primereact/button";
+import React, { useContext } from "react";
+import { useNavigate } from "react-router-dom";
+import LoadingBlockUI from "../../../../../../Library/LoadingBlockUI/LoadingBlockUI";
+import { RootStoreContext } from "../../../../../../RootStore";
+import InputMultiOrg from "../../../../../../Shared/InputEditors/InputMultiOrg";
+import InputOrg from "../../../../../../Shared/InputEditors/InputOrg";
+
+const FHaVSettingsOrgs = ({}) => {
+  const navigate = useNavigate();
+  const rootStore = useContext(RootStoreContext);
+  const {
+    fetchHa,
+    selectedHa,
+    isFetchingHa,
+    isUpdatingHa,
+    updateHa,
+    isHaRegistryCacheValid,
+  } = rootStore.haStore;
+
+  const { appVars } = rootStore.authStore;
+  if (isFetchingHa) {
+    return <Loading message={"Fetching Ha..."} />;
+  }
+
+  const onFormikSubmit = (data) => {
+    const updatedHa = { ...selectedHa, ...data };
+    console.log("updatedHa", updatedHa);
+    // keep only the ids of the participating orgs if they are available in appVars.orgs
+    updatedHa.participatingOrgs = updatedHa.participatingOrgs.filter(
+      (orgId) => appVars.orgs[orgId]
+    );
+
+    updateHa(updatedHa);
+  };
+
+  return (
+    <div className="flex min-w-full fadein animation-duration-500">
+      <div className="card w-full p-3">
+        <LoadingBlockUI blocked={isUpdatingHa}>
+          <Formik
+            initialValues={{
+              primaryOrgId: selectedHa?.primaryOrgId,
+              participatingOrgs: selectedHa?.participatingOrgs,
+            }}
+            onSubmit={onFormikSubmit}
+          >
+            {({
+              values,
+              errors,
+              touched,
+              handleChange,
+              handleBlur,
+              handleSubmit,
+              isSubmitting,
+              /* and other goodies */
+            }) => (
+              <form onSubmit={handleSubmit} className="p-fluid">
+                <div className="flex flex-column">
+                  <div className="field">
+                    <label htmlFor="primaryOrgId">Primary Organization</label>
+                    <InputOrg
+                      id="primaryOrgId"
+                      value={values.primaryOrgId}
+                      onChange={handleChange}
+                      onBlur={handleBlur}
+                    />
+                  </div>
+                  <div className="field">
+                    <label htmlFor="primaryOrgId">
+                      Participating Organization
+                    </label>
+                    <InputMultiOrg
+                      id="participatingOrgs"
+                      value={values.participatingOrgs}
+                      onChange={handleChange}
+                      onBlur={handleBlur}
+                    />
+                  </div>
+
+                  <div className="field">
+                    <Button
+                      icon="icon icon-common icon-database-submit"
+                      type="submit"
+                      label="Save Changes"
+                      className="p-button-secondary w-12rem"
+                      loading={isUpdatingHa}
+                    />
+                  </div>
+                </div>
+              </form>
+            )}
+          </Formik>
+        </LoadingBlockUI>
+      </div>
+    </div>
+  );
+};
+
+export default observer(FHaVSettingsOrgs);
