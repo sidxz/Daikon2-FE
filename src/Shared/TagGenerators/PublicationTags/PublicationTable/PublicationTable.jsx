f6ca9f14f497e20ddb87ffc0ea432d8e9e9ec320
Siddhant Rath <sid@tamu.edu>
2024-06-16 00:51:45 -0500

    Publications

---
+++ src/Shared/TagGenerators/PublicationTags/PublicationTable/PublicationTable.jsx
@@ -0,0 +1,119 @@
+import { observer } from "mobx-react-lite";
+import { Column } from "primereact/column";
+import { DataTable } from "primereact/datatable";
+import { Skeleton } from "primereact/skeleton";
+import React, { useEffect, useState } from "react";
+import { NavLink } from "react-router-dom";
+import * as Helper from "./PublicationTableHelper";
+const PublicationTable = ({ publications }) => {
+  const [pubData, setPubData] = useState(
+    publications.map((publication) => ({
+      ...publication,
+      item3:
+        publication.item2 === "PubMed" ? (
+          <Skeleton width="5rem" className="mb-2" />
+        ) : (
+          publication.item3
+        ),
+      item4:
+        publication.item2 === "PubMed" ? (
+          <Skeleton width="5rem" className="mb-2" />
+        ) : (
+          publication.item4
+        ),
+      item5:
+        publication.item2 === "PubMed" ? (
+          <Skeleton width="5rem" className="mb-2" />
+        ) : (
+          publication.item5
+        ),
+      item6:
+        publication.item2 === "PubMed" ? (
+          <Skeleton width="5rem" className="mb-2" />
+        ) : (
+          publication.item6
+        ),
+      loading:
+        publication.item2 === "PubMed" ? "Loading..." : publication.item3,
+    }))
+  );
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const updatedPublications = await Promise.all(
+        pubData.map(async (publication, index) => {
+          if (
+            publication.item2 === "PubMed" &&
+            publication.loading === "Loading..."
+          ) {
+            await Helper.delay(index * 1000);
+            const pubMedData = await Helper.fetchPubMedData(publication.item1);
+            return {
+              ...publication,
+              item3: pubMedData.title,
+              item4: <NavLink to={pubMedData.link}>{pubMedData.link}</NavLink>,
+              item5: pubMedData.pubDate,
+              item6: pubMedData.authors,
+              loading: pubMedData.title,
+            };
+          }
+          return publication;
+        })
+      );
+      setPubData(updatedPublications);
+      setLoading(false);
+    };
+
+    if (loading) {
+      fetchData();
+    }
+  }, [loading, pubData]);
+
+  return (
+    <div className="flex w-full">
+      <DataTable
+        value={pubData}
+        paginator
+        rows={30}
+        //loading={isGeneListLoading}
+        className="w-full"
+        emptyMessage="Loading..."
+      >
+        <Column
+          field="item1"
+          header="DOI"
+          //body={Helper.accessionNumberBodyTemplate}
+          className="narrow-column"
+        />
+        <Column
+          field="item2"
+          header="Source"
+          //body={Helper.accessionNumberBodyTemplate}
+          className="narrow-column"
+        />
+        <Column field="item3" header="Title" className="narrow-column" />
+        <Column
+          field="item4"
+          header="Url"
+          //body={Helper.accessionNumberBodyTemplate}
+          className="narrow-column"
+        />
+        <Column
+          field="item5"
+          header="Date"
+          //body={Helper.accessionNumberBodyTemplate}
+          className="narrow-column"
+        />
+        <Column
+          field="item6"
+          header="Other"
+          //body={Helper.accessionNumberBodyTemplate}
+          className="narrow-column"
+        />
+      </DataTable>
+    </div>
+  );
+};
+
+export default observer(PublicationTable);
--- src/Shared/TagGenerators/PublicationTags/PublicationTags.jsx
+++ src/Shared/TagGenerators/PublicationTags/PublicationTags.jsx
@@ -1,26 +1,54 @@
+import { Dialog } from "primereact/dialog";
 import { Tag } from "primereact/tag";
-import React from "react";
+import React, { useState } from "react";
 import { IoIosJournal } from "react-icons/io";
+import PublicationTable from "./PublicationTable/PublicationTable";
 
 const PublicationTags = ({ publications }) => {
+  const [visible, setVisible] = useState(false);
   if (!publications || publications.length === 0) {
     return <></>;
   }
-  return (
-    <Tag
-      style={{
-        background:
-          "linear-gradient(-225deg, #336699 0%, #5588bb 48%, #77aadd 100%)",
-      }}
-    >
-      <div className="flex align-items-center">
-        <IoIosJournal />
+  console.log("publications", publications);
 
-        <span className="text-xs">
-          {publications.length} Publication{publications.length > 1 ? "s" : ""}
-        </span>
-      </div>
-    </Tag>
+  const headerElement = (
+    <div className="inline-flex align-items-center justify-content-center gap-2">
+      <span className="font-bold white-space-nowrap">Publications</span>
+    </div>
+  );
+
+  return (
+    <>
+      <Tag
+        style={{
+          background:
+            "linear-gradient(-225deg, #336699 0%, #5588bb 48%, #77aadd 100%)",
+        }}
+        className="cursor-pointer"
+        onClick={() => setVisible(true)}
+      >
+        <div className="flex align-items-center">
+          <IoIosJournal />
+
+          <span className="text-xs">
+            {publications.length} Publication
+            {publications.length > 1 ? "s" : ""}
+          </span>
+        </div>
+      </Tag>
+      <Dialog
+        visible={visible}
+        modal
+        header={headerElement}
+        style={{ width: "90rem" }}
+        onHide={() => {
+          if (!visible) return;
+          setVisible(false);
+        }}
+      >
+        <PublicationTable publications={publications} />
+      </Dialog>
+    </>
   );
 };
 
