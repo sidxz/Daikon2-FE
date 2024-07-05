fc57f100e561b128fffaccd5b1dc77cd9ae4c9db
Saswati Panda <panda.sas20@gmail.com>
2024-07-05 13:42:03 -0500

    apconix grid div layout

--- src/Apps/Flow/FlowTarget/FTViewer/FTVApconix/FTVApconixGrid.jsx
+++ src/Apps/Flow/FlowTarget/FTViewer/FTVApconix/FTVApconixGrid.jsx
@@ -1,45 +1,125 @@
 import React from "react";
 import "./FTVApconixGrid.css";
+import { FcPlanner } from "react-icons/fc";
 
 const FTVApconixGrid = () => {
-    const data = [
-        { name: "Carcinogenesis", risk: "", impact: "", likelihood: "" },
-        { name: "Cardiovascular", risk: "", impact: "", likelihood: "" },
-        { name: "Endocrine", risk: "", impact: "", likelihood: "" },
-        { name: "Gastrointestinal", risk: "", impact: "", likelihood: "" },
-        { name: "Hematological and Immune", risk: "", impact: "", likelihood: "" },
-        { name: "Hepatobiliary ", risk: "", impact: "", likelihood: "" },
-        { name: "Integumentary ", risk: "", impact: "", likelihood: "" },
-        { name: "Nervous", risk: "", impact: "", likelihood: "" },
-        { name: "Embryofetal Developmental Toxicity", risk: "", impact: "", likelihood: "" },
-        { name: "Reproductive Female ", risk: "", impact: "", likelihood: "" },
-        { name: "Reproductive Male ", risk: "", impact: "", likelihood: "" },
-        { name: "Respiratory", risk: "", impact: "", likelihood: "" },
-        { name: "Sensory", risk: "", impact: "", likelihood: "" },
-        { name: "Urinary", risk: "", impact: "", likelihood: "" },
-      ];
   return (
-    <div className="apconixGrid">
-      <table className="apconixGrid">
-      <thead>
-        <tr>
-          <th></th>
-          <th>Risk</th>
-          <th>Impact</th>
-          <th>Likelihood (Likelihood of risk occurring assuming mammalian target modulation)</th>
-        </tr>
-      </thead>
-      <tbody>
-        {data.map((row, index) => (
-          <tr key={index}>
-            <td>{row.name}</td>
-            <td>{row.risk}</td>
-            <td>{row.impact}</td>
-            <td>{row.likelihood}</td>
-          </tr>
-        ))}
-      </tbody>
-    </table>
+    <div className="flex">
+      <div className="flex">
+        <div className="flex flex-column">
+          <div className="flex w-1 p-2 align-items-center bg-white">&nbsp;</div>
+          <div className="flex">
+            <div className="flex w-full p-2 align-items-center justify-content-center bg-teal-400 text-white">
+              Carcinogenesis
+            </div>
+          </div>
+          <div className="flex w-full pt-2">
+            <div className="flex w-full p-2 align-items-center justify-content-center bg-teal-400 text-white">
+              Cardiovascular
+            </div>
+          </div>
+          <div className="flex w-full pt-2">
+            <div className="flex w-full p-2 align-items-center justify-content-center bg-teal-400 text-white">
+              Endocrine
+            </div>
+          </div>
+          <div className="flex w-full pt-2">
+            <div className="flex w-full p-2  align-items-center justify-content-center bg-teal-400 text-white">
+              Gastrointestinal
+            </div>
+          </div>
+          <div className="flex w-full pt-2">
+            <div className="flex w-full p-2  align-items-center justify-content-center bg-teal-400 text-white">
+              Hematological and Immune
+            </div>
+          </div>
+          <div className="flex w-full pt-2">
+            <div className="flex w-full p-2  align-items-center justify-content-center bg-teal-400 text-white">
+              Hepatobiliary
+            </div>
+          </div>
+          <div className="flex w-full pt-2">
+            <div className="flex w-full p-2  align-items-center justify-content-center bg-teal-400 text-white">
+              Integumentary
+            </div>
+          </div>
+          <div className="flex w-full pt-2">
+            <div className="flex w-full p-2  align-items-center justify-content-center bg-teal-400 text-white">
+              Nervous
+            </div>
+          </div>
+          <div className="flex w-full pt-2">
+            <div className="flex w-full p-2  align-items-center justify-content-center bg-teal-400 text-white">
+              Embryofetal Developmental Toxicity
+            </div>
+          </div>
+          <div className="flex w-full pt-2">
+            <div className="flex w-full p-2  align-items-center justify-content-center bg-teal-400 text-white">
+              Reproductive Female
+            </div>
+          </div>
+          <div className="flex w-full pt-2">
+            <div className="flex w-full p-2  align-items-center justify-content-center bg-teal-400 text-white">
+              Reproductive Male
+            </div>
+          </div>
+          <div className="flex w-full pt-2">
+            <div className="flex w-full p-2  align-items-center justify-content-center bg-teal-400 text-white">
+              Respiratory
+            </div>
+          </div>
+          <div className="flex w-full pt-2">
+            <div className="flex w-full p-2  align-items-center justify-content-center bg-teal-400 text-white">
+              Sensory
+            </div>
+          </div>
+
+          <div className="flex w-full pt-2">
+            <div className="flex w-full p-2  align-items-center justify-content-center bg-teal-400 text-white">
+              Urinary
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="flex w-full surface-0">
+        <div className="flex w-1 p-4 align-items-center bg-white">&nbsp;</div>
+        <div className="flex w-3 text-sm">
+          <div
+            className="flex text-lg"
+            style={{
+              color: "#3c83bd",
+            }}
+          >
+            <div className="flex">
+              <b>Risk</b>
+            </div>
+          </div>
+        </div>
+        <div className="flex w-3 text-sm ">
+          <div
+            className="flex text-lg"
+            style={{
+              color: "#3c83bd",
+            }}
+          >
+            <div className="flex">
+              <b>Impact</b>
+            </div>
+          </div>
+        </div>
+        <div className="flex w-3 text-sm">
+          <div
+            className="flex text-lg"
+            style={{
+              color: "#3c83bd",
+            }}
+          >
+            <div className="flex">
+              <b>Likelihood </b>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
