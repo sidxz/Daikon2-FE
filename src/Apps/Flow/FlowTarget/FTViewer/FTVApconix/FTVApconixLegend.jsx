29a59f32050d9445151447e232917278b2b26069
Saswati Panda <panda.sas20@gmail.com>
2024-07-05 17:46:15 -0500

    legend position

--- src/Apps/Flow/FlowTarget/FTViewer/FTVApconix/FTVApconixLegend.jsx
+++ src/Apps/Flow/FlowTarget/FTViewer/FTVApconix/FTVApconixLegend.jsx
@@ -36,6 +36,18 @@ const FTVApconixLegend = () => {
         Drug binding domain not known, less than 1000 X in vitro selectivity toward Mtb target demonstrated, or mammalian screen not feasible
         </div>
       </div>
+      <div className="flex text-sm font-italic text-blue-900 border-1">
+      Mammalian off-target risk profile. Risks are rated by Impact/Likelihood (H/M/L – High/Moderate/Low) across each organ system and process: carcinogenesis (CA), 
+      cardiovascular (CV), endocrine (EN), gastrointestinal (GI), hematological and immune (HI), hepatobiliary (HB), integumentary (IN), nervous (NS), 
+      EFD embryofetal developmental toxicity (EFD) reproductive female (RF), reproductive male (RM), respiratory (RI), sensory (SE), urinary (UR).
+      Key (High-Moderate impact and likelihood) risks that should be prioritized for derisking or assessment in exploratory toxicology studies and in  the standard toxicology program are 
+      indicated by
+      <span className="sticky"><FaGaugeSimpleHigh /></span>
+      </div>
+      
+        
+     
+      
     </div>
   );
 };
