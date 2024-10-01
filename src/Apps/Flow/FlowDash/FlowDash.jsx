4aac3c94ec592865b30e7edd976138233b59c199
Siddhant Rath <sid@tamu.edu>
2024-10-01 16:22:41 -0500

    Mlogix Search Widget

--- src/Apps/Flow/FlowDash/FlowDash.jsx
+++ src/Apps/Flow/FlowDash/FlowDash.jsx
@@ -4,6 +4,7 @@ import React from "react";
 import MostRecentComments from "../../Comments/MostRecentComments/MostRecentComments";
 import "./FlowDash.css";
 import FlowDashCards from "./FlowDashCards/FlowDashCards";
+import FlowDashMLogixSearch from "./FlowDashMLogixSearch/FlowDashMLogixSearch";
 import FlowDashWhatsNew from "./FlowDashWhatsNew/FlowDashWhatsNew";
 const FlowDash = () => {
   let generateCardTitle = (icon, title) => {
@@ -46,18 +47,33 @@ const FlowDash = () => {
       </div>
 
       <div className="flex w-full gap-2 mt-4">
-        <div className="flex flex-column p-2 w-8">
-          <Card
-            title={generateCardTitle("ri-discuss-line", "Recent Discussions")}
-            className="w-full min-h-full"
-          >
-            <div className="flex w-full pt-1">
-              <MostRecentComments />
-            </div>
-          </Card>
+        <div className="flex flex-column w-full w-8 gap-2 p-1">
+          <div className="flex flex-column">
+            <Card
+              title={generateCardTitle(
+                "icon icon-common icon-math",
+                "Molecule Search"
+              )}
+              className="w-full min-h-full"
+            >
+              <div className="flex w-full pt-1">
+                <FlowDashMLogixSearch />
+              </div>
+            </Card>
+          </div>
+          <div className="flex flex-column">
+            <Card
+              title={generateCardTitle("ri-discuss-line", "Recent Discussions")}
+              className="w-full min-h-full"
+            >
+              <div className="flex w-full pt-1">
+                <MostRecentComments />
+              </div>
+            </Card>
+          </div>
         </div>
 
-        <div className="flex flex-column p-2 w-4">
+        <div className="flex flex-column p-1 w-4">
           <Card
             title={generateCardTitle(
               "icon icon-common icon-newspaper",
---
+++ src/Apps/Flow/FlowDash/FlowDashMLogixSearch/FlowDashMLogixSearch.jsx
@@ -0,0 +1,160 @@
+import { observer } from "mobx-react-lite";
+import { Button } from "primereact/button";
+import { Dialog } from "primereact/dialog";
+import { InputText } from "primereact/inputtext";
+import { RadioButton } from "primereact/radiobutton";
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import JSMEditor from "../../../../Library/JSME/JSMEditor";
+import { MolecuLogixIcon } from "../../../MolecuLogix/Icons/MolecuLogixIcon";
+
+const FlowDashMLogixSearch = () => {
+  const [searchValue, setSearchValue] = useState(""); // Search value (SMILES or Name)
+  const [searchType, setSearchType] = useState("substructure"); // Search type (substructure, similarity, exact, name)
+  const [similarityThreshold, setSimilarityThreshold] = useState(90); // Similarity threshold (only used for similarity search)
+  const [searchLimit, setSearchLimit] = useState(100); // Search limit
+  const [showStructureEditor, setShowStructureEditor] = useState(false); // Structure editor dialog visibility
+  const navigate = useNavigate(); // Navigation hook from react-router-dom
+
+  // Function to handle the search button click
+  const handleSearch = () => {
+    const params = {
+      smiles: searchValue,
+      searchType: searchType,
+      limit: searchLimit,
+    };
+
+    // Add similarity threshold only if the search type is similarity
+    if (searchType === "similarity") {
+      params.threshold = similarityThreshold;
+    }
+
+    // Construct query string from params
+    const queryParams = new URLSearchParams(params).toString();
+
+    // Navigate to the search results page with the query params
+    navigate(`/moleculogix/search/?${queryParams}`);
+  };
+
+  return (
+    <>
+      <div className="flex w-full flex-column gap-1 p-3">
+        {/* Search Input and Buttons */}
+        <div className="flex w-full align-items-center gap-3">
+          <div className="flex-grow-1">
+            <InputText
+              className="w-full text-lg"
+              placeholder="Enter search query: SMILES or Name"
+              value={searchValue}
+              onChange={(e) => setSearchValue(e.target.value)}
+            />
+          </div>
+          <div className="flex">
+            <Button
+              text
+              type="p-button"
+              icon={<MolecuLogixIcon size={28} />}
+              label="Structure Editor"
+              onClick={() => setShowStructureEditor(true)}
+            />
+          </div>
+          <div>
+            <Button
+              className="p-button"
+              label="Search"
+              size="medium"
+              disabled={searchValue === ""}
+              onClick={handleSearch} // Trigger the handleSearch function on click
+            />
+          </div>
+        </div>
+
+        {/* Radio Buttons for Search Type */}
+        <div className="flex w-full border-1 border-50 border-round-md p-3 align-items-center">
+          <div className="flex-grow-1 flex gap-4">
+            <div className="flex align-items-center">
+              <RadioButton
+                inputId="substructure"
+                name="searchType"
+                value="substructure"
+                onChange={(e) => setSearchType(e.value)}
+                checked={searchType === "substructure"}
+              />
+              <label htmlFor="substructure" className="ml-2">
+                Substructure
+              </label>
+            </div>
+            <div className="flex align-items-center">
+              <RadioButton
+                inputId="similarity"
+                name="searchType"
+                value="similarity"
+                onChange={(e) => setSearchType(e.value)}
+                checked={searchType === "similarity"}
+              />
+              <label htmlFor="similarity" className="ml-2">
+                Similarity
+              </label>
+            </div>
+            <div className="flex align-items-center">
+              <RadioButton
+                inputId="exact"
+                name="searchType"
+                value="exact"
+                onChange={(e) => setSearchType(e.value)}
+                checked={searchType === "exact"}
+              />
+              <label htmlFor="exact" className="ml-2">
+                Exact
+              </label>
+            </div>
+            <div className="flex align-items-center">
+              <RadioButton
+                inputId="name"
+                name="searchType"
+                value="name"
+                onChange={(e) => setSearchType(e.value)}
+                checked={searchType === "name"}
+              />
+              <label htmlFor="name" className="ml-2">
+                By Name
+              </label>
+            </div>
+          </div>
+          <div className="flex align-items-center gap-2">
+            <label className="text-color-secondary text-sm">
+              Limit Results
+            </label>
+            <InputText
+              className="text-sm w-4rem p-1"
+              keyfilter="int"
+              value={searchLimit}
+              onChange={(e) => setSearchLimit(Number(e.target.value))}
+            />
+          </div>
+        </div>
+      </div>
+
+      {/* Structure Editor Dialog */}
+      <Dialog
+        visible={showStructureEditor}
+        closable={false}
+        modal={false}
+        showHeader={false}
+        onHide={() => setShowStructureEditor(false)}
+      >
+        <div className="flex pt-5">
+          <JSMEditor
+            initialSmiles={searchValue}
+            onSave={(s) => {
+              setShowStructureEditor(false);
+              setSearchValue(s); // Set the SMILES from the structure editor
+            }}
+          />
+        </div>
+      </Dialog>
+    </>
+  );
+};
+
+export default observer(FlowDashMLogixSearch);
