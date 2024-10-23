e191327253d80b1d8532f91a6eca5b21a9126076
Saswati Panda <panda.sas20@gmail.com>
2024-10-23 14:53:36 -0500

    icon update

--- src/Apps/Flow/FlowDash/FlowDashEventUpdates/FlowDashEventUpdates.jsx
+++ src/Apps/Flow/FlowDash/FlowDashEventUpdates/FlowDashEventUpdates.jsx
@@ -4,18 +4,24 @@ import React from "react";
 import MostRecentEvents from "../../../Events/MostRecentEvents/MostRecentEvents";
 
 const FlowDashEventUpdates = () => {
-  let cardTitle = (text) => (
+  let cardTitle = (icon, text) => (
     <div
       className="flex justify-content-center m-0 p-0"
       style={{ margin: "0px", marginBottom: "-10px" }}
     >
+      <div className="flex p-1  mr-1 text-cyan-700 text-2xl">
+        <i className={icon} />
+      </div>
       <div className="m-0 p-0 text-cyan-700 text-2xl">{text}</div>
     </div>
   );
 
   return (
     <div className="flex w-full surface-50 border-round m-0 p-0 gap-2">
-      <Card className="w-full" title={cardTitle("Recent Activity")}>
+      <Card
+        className="w-full"
+        title={cardTitle("icon icon-common icon-clock", "Recent Activity")}
+      >
         <MostRecentEvents />
       </Card>
     </div>
