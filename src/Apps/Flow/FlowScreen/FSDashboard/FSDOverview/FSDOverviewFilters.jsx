52d64c2339b81ec98cc21a2583ce6e66b7c4bec9
Siddhant Rath <sid@tamu.edu>
2024-10-29 11:20:22 -0500

    filters

--- src/Apps/Flow/FlowScreen/FSDashboard/FSDOverview/FSDOverviewFilters.jsx
+++ src/Apps/Flow/FlowScreen/FSDashboard/FSDOverview/FSDOverviewFilters.jsx
@@ -1,9 +1,13 @@
 import { observer } from "mobx-react-lite";
+import { Dropdown } from "primereact/dropdown";
 import { MultiSelect } from "primereact/multiselect";
 import { Toolbar } from "primereact/toolbar";
 import React, { useContext, useEffect, useState } from "react";
+import { MdGridView } from "react-icons/md";
 import { useSearchParams } from "react-router-dom";
 import { RootStoreContext } from "../../../../../RootStore";
+import { PhenoScreenIcon } from "../../../icons/PhenoScreenIcon";
+import { TargetIcon } from "../../../icons/TargetIcon";
 import "./FSDOverviewFilters.css";
 
 const FSDOverviewFilters = () => {
@@ -15,6 +19,7 @@ const FSDOverviewFilters = () => {
   const filterOptions = getFilterAttributes();
   const [dates, setDates] = useState(filterCriteria.dateRange || [null, null]);
   const [isInitialized, setIsInitialized] = useState(false); // Track initialization
+  const [dashDisplay, setDashDisplay] = useState("All");
 
   // Initialize filters from URL params on component mount
   useEffect(() => {
@@ -68,8 +73,22 @@ const FSDOverviewFilters = () => {
     }
   }, [filterCriteria, isInitialized, setSearchParams]);
 
+  const viewOptions = [
+    {
+      value: "All",
+      icon: (
+        <>
+          <TargetIcon />
+          <PhenoScreenIcon />
+        </>
+      ),
+    },
+    { value: "Target Based", icon: <TargetIcon /> },
+    { value: "Phenotypic", icon: <PhenoScreenIcon /> },
+  ];
+
   const centerContent = (
-    <React.Fragment>
+    <div className="flex border-0 border-50 border-round-md">
       <MultiSelect
         value={filterCriteria.targets}
         onChange={(e) => setFilterCriteria({ targets: e.value })}
@@ -103,12 +122,28 @@ const FSDOverviewFilters = () => {
         display="chip"
         className="border-0 w-full"
       />
-    </React.Fragment>
+      <Dropdown
+        icon="pi pi-check"
+        className="border-0 p-0 m-0"
+        value={dashDisplay}
+        onChange={(e) => setDashDisplay(e.value)}
+        options={viewOptions}
+        optionValue="value"
+        optionLabel="value"
+        valueTemplate={(option) => {
+          return <MdGridView />;
+        }}
+      />
+    </div>
   );
 
   return (
-    <div className="div border-0 w-full m-0 p-0">
-      <Toolbar center={centerContent} className="m-0 p-0 border-0" />
+    <div className="div border-0 w-full m-0 p-0 ">
+      <Toolbar
+        end={centerContent}
+        //end={endContent}
+        className="m-0 p-0 border-0"
+      />
     </div>
   );
 };
