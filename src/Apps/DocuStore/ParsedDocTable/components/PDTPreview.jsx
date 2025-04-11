816c0ec1f57a27278b383a130afcd0463c94db2a
Saswati Panda <panda.sas20@gmail.com>
2025-04-11 17:02:55 -0500

    inplace component

--- src/Apps/DocuStore/ParsedDocTable/components/PDTPreview.jsx
+++ src/Apps/DocuStore/ParsedDocTable/components/PDTPreview.jsx
@@ -5,14 +5,13 @@ import MLTags from "../../../../Shared/TagGenerators/MLTags/MLTags";
 import { Rating } from "primereact/rating";
 import { Button } from "primereact/button";
 import { InputTextarea } from "primereact/inputtextarea";
+import { Divider } from "primereact/divider";
+import { Dialog } from "primereact/dialog";
 
 const PDTPreview = ({ rowData }) => {
-  const [showCommentBox, setShowCommentBox] = useState(false);
-  const [showIssueBox, setShowIssueBox] = useState(false);
   const [value, setValue] = useState(null);
+  const [visible, setVisible] = useState(false);
 
-  const [comment, setComment] = useState("");
-  const [issue, setIssue] = useState("");
   return (
     <div className="flex flex-column gap-1">
       <div className="flex align-content-center">
@@ -38,7 +37,7 @@ const PDTPreview = ({ rowData }) => {
           <MLTags entity={rowData?.shortSummary} />
         </div>
       )}
-      <div className="flex m-1 p-2 mt-4">
+      <div className="flex m-1 p-1 mt-4 mb-0">
         <div className="flex flex-column text-xs">
           <span className="text-gray-600 pb-2">Relevance</span>
           <Rating
@@ -55,23 +54,130 @@ const PDTPreview = ({ rowData }) => {
             cancel={false}
           />
         </div>
-        <div className="flex flex-column p-3">
+        <div className="flex flex-column ml-3 mt-3 p-0">
           <Button
             icon="pi pi-comment"
             label="4"
             className="p-button-text p-button-sm"
             text
+            onClick={() => setVisible(true)}
           />
+          <Dialog
+            header="Summary Preview"
+            visible={visible}
+            maximizable
+            style={{ width: "50vw" }}
+            onHide={() => {
+              if (!visible) return;
+              setVisible(false);
+            }}
+          >
+            <p className="m-0">
+              <div className="flex text-justify	line-height-3 select-text">
+                {DVariableResolver(rowData?.shortSummary)?.replace(/\*/g, " ")}
+              </div>
+              {rowData?.shortSummary?.value?.length > 0 && (
+                <div className="flex border-0">
+                  <MLTags entity={rowData?.shortSummary} />
+                </div>
+              )}{" "}
+            </p>
+          </Dialog>
         </div>
-        <div className="flex flex-column p-3">
+        <div className="flex flex-column ml-3 mt-3 p-0">
           <Button
             label="2"
             icon="pi pi-exclamation-triangle"
             className="p-button-text p-button-sm"
             text
+            onClick={() => setVisible(true)}
           />
+          <Dialog
+            header="Summary Preview"
+            visible={visible}
+            maximizable
+            style={{ width: "50vw" }}
+            onHide={() => {
+              if (!visible) return;
+              setVisible(false);
+            }}
+          >
+            <p className="m-0">
+              <div className="flex text-justify	line-height-3 select-text">
+                {DVariableResolver(rowData?.shortSummary)?.replace(/\*/g, " ")}
+              </div>
+              {rowData?.shortSummary?.value?.length > 0 && (
+                <div className="flex border-0">
+                  <MLTags entity={rowData?.shortSummary} />
+                </div>
+              )}{" "}
+            </p>
+          </Dialog>
+        </div>
+
+        <div className="flex flex-column ml-3 mt-3 p-0">
+          <Button
+            
+            label="+ New Comment"
+            className="p-button-text p-button-sm"
+            text
+            onClick={() => setVisible(true)}
+          />
+          <Dialog
+            header="Summary Preview"
+            visible={visible}
+            maximizable
+            style={{ width: "50vw" }}
+            onHide={() => {
+              if (!visible) return;
+              setVisible(false);
+            }}
+          >
+            <p className="m-0">
+              <div className="flex text-justify	line-height-3 select-text">
+                {DVariableResolver(rowData?.shortSummary)?.replace(/\*/g, " ")}
+              </div>
+              {rowData?.shortSummary?.value?.length > 0 && (
+                <div className="flex border-0">
+                  <MLTags entity={rowData?.shortSummary} />
+                </div>
+              )}{" "}
+            </p>
+          </Dialog>
+        </div>
+
+        <div className="flex flex-column ml-3 mt-3 p-0">
+          <Button
+            
+            label="+ New Issue"
+            className="p-button-text p-button-sm"
+            text
+            onClick={() => setVisible(true)}
+          />
+          <Dialog
+            header="Summary Preview"
+            visible={visible}
+            maximizable
+            style={{ width: "50vw" }}
+            onHide={() => {
+              if (!visible) return;
+              setVisible(false);
+            }}
+          >
+            <p className="m-0">
+              <div className="flex text-justify	line-height-3 select-text">
+                {DVariableResolver(rowData?.shortSummary)?.replace(/\*/g, " ")}
+              </div>
+              {rowData?.shortSummary?.value?.length > 0 && (
+                <div className="flex border-0">
+                  <MLTags entity={rowData?.shortSummary} />
+                </div>
+              )}{" "}
+            </p>
+          </Dialog>
         </div>
       </div>
+      <Divider className="mt-0" />
     </div>
   );
 };
