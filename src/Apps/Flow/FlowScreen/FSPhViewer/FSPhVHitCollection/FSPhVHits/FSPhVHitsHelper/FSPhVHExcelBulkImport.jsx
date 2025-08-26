2c7ad8113ad4c8e9687d992d67b3eed9cd3ccd61
Saswati Panda <panda.sas20@gmail.com>
2025-08-26 16:29:21 -0500

    Metadata for bulk uploads

--- src/Apps/Flow/FlowScreen/FSPhViewer/FSPhVHitCollection/FSPhVHits/FSPhVHitsHelper/FSPhVHExcelBulkImport.jsx
+++ src/Apps/Flow/FlowScreen/FSPhViewer/FSPhVHitCollection/FSPhVHits/FSPhVHitsHelper/FSPhVHExcelBulkImport.jsx
@@ -1,12 +1,16 @@
 import { observer } from "mobx-react-lite";
+import { Button } from "primereact/button";
 import { Dialog } from "primereact/dialog";
 import { FileUpload } from "primereact/fileupload";
-import { useContext, useState } from "react";
+import { OverlayPanel } from "primereact/overlaypanel";
+import { useContext, useRef, useState } from "react";
 import { STRINGS } from "../../../../../../../Customizations/strings";
 import BulkDataImportProgress from "../../../../../../../Library/BulkDataImportProgress/BulkDataImportProgress";
 import SmilesView from "../../../../../../../Library/SmilesView/SmilesView";
 import { RootStoreContext } from "../../../../../../../RootStore";
 import ImportFromExcel from "../../../../../../../Shared/Excel/ImportFromExcel";
+import InputOrg from "../../../../../../../Shared/InputEditors/InputOrg";
+import InputScientist from "../../../../../../../Shared/InputEditors/InputScientist";
 import { DoseResponseBodyTemplate } from "../../../../FSTbViewer/FSTbVHitCollection/FSTbVHits/FSTbVHitsHelper/FSTbVHDataTableHelper";
 import { GroupMolecules } from "../../../../shared/DataImportHelper";
 import { isSameMoleculeName } from "../../../../shared/SharedHelper";
@@ -21,11 +25,18 @@ const FSPhVHExcelBulkImport = ({
   visible,
   onHide,
 }) => {
+  const rootStore = useContext(RootStoreContext);
+  const { user } = rootStore.authStore;
+
   const [dataForPreview, setDataForPreview] = useState([]);
   const [showDataPreviewDialog, setShowDataPreviewDialog] = useState(false);
+  const metaOverlayPanel = useRef(null);
+  const [metaDataScientist, setMetaDataScientist] = useState(
+    user?.firstName + " " + user?.lastName
+  );
+  const [metaDataOrgID, setMetaDataOrgID] = useState(user?.appOrgId);
 
   if (visible) {
-    const rootStore = useContext(RootStoreContext);
     const {
       bulkInsertHits,
       isBulkUploadingHits,
@@ -95,88 +106,130 @@ const FSPhVHExcelBulkImport = ({
         onHide={onHide}
         closable
       >
-        <FileUpload
-          name="excelFile"
-          accept=".xls,.xlsx,application/vnd.ms-excel,application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
-          maxFileSize={1000000}
-          mode="advanced"
-          chooseLabel="Browse and Select File"
-          chooseOptions={{
-            icon: "icon icon-common icon-plus-circle",
-            className: "p-button-text p-button-md",
-          }}
-          cancelOptions={{
-            label: "Cancel",
-            icon: "pi pi-times",
-            className: "p-button-danger",
-          }}
-          className="p-button-text p-button-secondary"
-          customUpload={true}
-          emptyTemplate={emptyTemplate}
-          uploadHandler={async (e) => {
-            let file = e.files[0];
-            const jsonData = await ImportFromExcel({
-              file: file,
-              headerMap: DtFieldsToExcelColumnMapping,
-            });
-            e.files = null;
+        <div className="flex flex-column w-full gap-1">
+          <div className="flex align-items-right justify-content-end">
+            <Button
+              className="p-button-text p-button-secondary"
+              label="Edit Metadata"
+              onClick={(e) => metaOverlayPanel.current.toggle(e)}
+            />
+            <OverlayPanel ref={metaOverlayPanel}>
+              <div className="flex flex-column gap-2">
+                <div className="flex align-items-center gap-2">
+                  <div className="flex flex-none w-12rem">
+                    Disclosure Scientist
+                  </div>
+                  <div className="flex-auto">
+                    <InputScientist
+                      id="scientist"
+                      value={metaDataScientist}
+                      onChange={(e) => setMetaDataScientist(e.target.value)}
+                    />
+                  </div>
+                </div>
 
-            // This is to clear the file list in the FileUpload component
-            e.options.clear();
-            // console.log("jsonData", jsonData);
-            jsonData.forEach((row) => {
-              // row.hitCollectionId = selectedHitCollection.id;
-              // output is in field 'smiles' in excel (template), but to create a hit, we need 'requestedSMILES'
-              // row.requestedSMILES = row.smiles;
-              row = {
-                ...existingData.find((hit) => hit.id === row.id),
-                ...row,
-              };
-              // console.log("row", row);
-            });
-            console.log("jsonData", jsonData);
+                <div className="flex align-items-center gap-2">
+                  <div className="flex flex-none w-12rem">Disclosure Org</div>
+                  <div className="flex-auto">
+                    <InputOrg
+                      id="org"
+                      value={metaDataOrgID}
+                      onChange={(e) => setMetaDataOrgID(e.target.value)}
+                    />
+                  </div>
+                </div>
+              </div>
+            </OverlayPanel>
+          </div>
+          <div className="flex w-full">
+            <FileUpload
+              name="excelFile"
+              accept=".xls,.xlsx,application/vnd.ms-excel,application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
+              maxFileSize={1000000}
+              mode="advanced"
+              chooseLabel="Browse and Select File"
+              chooseOptions={{
+                icon: "icon icon-common icon-plus-circle",
+                className: "p-button-text p-button-md",
+              }}
+              cancelOptions={{
+                label: "Cancel",
+                icon: "pi pi-times",
+                className: "p-button-danger",
+              }}
+              className="p-button-text p-button-secondary w-full"
+              customUpload={true}
+              emptyTemplate={emptyTemplate}
+              uploadHandler={async (e) => {
+                let file = e.files[0];
+                const jsonData = await ImportFromExcel({
+                  file: file,
+                  headerMap: DtFieldsToExcelColumnMapping,
+                });
+                e.files = null;
 
-            let jsonDataGrouped = GroupMolecules(jsonData);
-            console.log("jsonDataGrouped", jsonDataGrouped);
+                // This is to clear the file list in the FileUpload component
+                e.options.clear();
+                // console.log("jsonData", jsonData);
+                jsonData.forEach((row) => {
+                  // row.hitCollectionId = selectedHitCollection.id;
+                  // output is in field 'smiles' in excel (template), but to create a hit, we need 'requestedSMILES'
+                  // row.requestedSMILES = row.smiles;
+                  row = {
+                    ...existingData.find((hit) => hit.id === row.id),
+                    ...row,
+                  };
+                  // console.log("row", row);
+                });
+                console.log("jsonData", jsonData);
 
-            setDataForPreview(jsonDataGrouped);
-            setShowDataPreviewDialog(true);
-            //hideFileUploadDialog();
-          }}
-          auto
-        />
-        <BulkDataImportProgress
-          headerMap={DtFieldsGroupedColumnMapping}
-          existingData={existingData}
-          comparatorKey="id"
-          comparatorFn={isSameMoleculeName}
-          structureFields={["smiles"]}
-          requiredFields={["moleculeName"]}
-          data={dataForPreview}
-          visible={showDataPreviewDialog}
-          onHide={() => {
-            setShowDataPreviewDialog(false);
-            setDataForPreview(null);
-          }}
-          onProceed={(rowsToSave) => {
-            // keep the dialog open to show live progress
-            //console.log(rowsToSave);
-            bulkInsertHits(rowsToSave, { batchSize: 100 });
-          }}
-          onCancel={() => cancelBulkUpload()}
-          //onSave={(data) => console.log("data", data)}
-          isUploading={isBulkUploadingHits}
-          bulkProgress={bulkProgress}
-          fieldFlatteners={{
-            doseResponses: DoseResponsesFlattener,
-          }}
-          customBodyTemplates={{
-            smiles: (rowData) => structureBodyTemplate(rowData),
-            doseResponses: (rowData) =>
-              DoseResponseBodyTemplate(rowData._originalRow),
-            // You can add more templates per field easily
-          }}
-        />
+                let jsonDataGrouped = GroupMolecules(jsonData);
+                console.log("jsonDataGrouped", jsonDataGrouped);
+
+                setDataForPreview(jsonDataGrouped);
+                setShowDataPreviewDialog(true);
+                //hideFileUploadDialog();
+              }}
+              auto
+            />
+            <BulkDataImportProgress
+              headerMap={DtFieldsGroupedColumnMapping}
+              existingData={existingData}
+              comparatorKey="id"
+              comparatorFn={isSameMoleculeName}
+              structureFields={["smiles"]}
+              requiredFields={["moleculeName"]}
+              data={dataForPreview}
+              visible={showDataPreviewDialog}
+              onHide={() => {
+                setShowDataPreviewDialog(false);
+                setDataForPreview(null);
+              }}
+              onProceed={(rowsToSave) => {
+                rowsToSave.forEach((row) => {
+                  row.disclosureScientist = metaDataScientist;
+                  row.disclosureOrgId = metaDataOrgID;
+                });
+                // keep the dialog open to show live progress
+                //console.log(rowsToSave);
+                bulkInsertHits(rowsToSave, { batchSize: 100 });
+              }}
+              onCancel={() => cancelBulkUpload()}
+              //onSave={(data) => console.log("data", data)}
+              isUploading={isBulkUploadingHits}
+              bulkProgress={bulkProgress}
+              fieldFlatteners={{
+                doseResponses: DoseResponsesFlattener,
+              }}
+              customBodyTemplates={{
+                smiles: (rowData) => structureBodyTemplate(rowData),
+                doseResponses: (rowData) =>
+                  DoseResponseBodyTemplate(rowData._originalRow),
+                // You can add more templates per field easily
+              }}
+            />
+          </div>
+        </div>
       </Dialog>
     );
   }
